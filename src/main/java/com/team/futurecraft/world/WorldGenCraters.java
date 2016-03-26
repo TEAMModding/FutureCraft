aafe6b01f7afe4ad9ce237ce9aeaf1f06d665456
pianoman373 <jholley373@yahoo.com>
2016-03-26 14:28:10 -0500

    Missed one.

--- src/main/java/com/team/futurecraft/world/WorldGenCraters.java
+++ src/main/java/com/team/futurecraft/world/WorldGenCraters.java
@@ -2,9 +2,10 @@ package com.team.futurecraft.world;
 
 import java.util.Random;
 
+import com.team.futurecraft.Vec3f;
+
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockPos;
-import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
@@ -27,14 +28,14 @@ public class WorldGenCraters extends WorldGenerator {
 	
 	public void placeCrater(World world, int x, int y, int z, int size) {
 		int dimensions = size * 2 + 1;
-		Vec3 centerVec = new Vec3((double)x, (double)y, (double)z);
+		Vec3f centerVec = new Vec3f((double)x, (double)y, (double)z);
 		for (int xCount = 0; xCount < dimensions; xCount++) {
 			for (int yCount = size + 1; yCount > 0; yCount--) {
 				for (int zCount = 0; zCount < dimensions; zCount++) {
 					int newX = xCount + x - size;
 					int newY = yCount + y - size;
 					int newZ = zCount + z - size;
-					Vec3 posVec = new Vec3((double)newX, (double)newY, (double)newZ);
+					Vec3f posVec = new Vec3f((double)newX, (double)newY, (double)newZ);
 					if (centerVec.distanceTo(posVec) < size && !world.isAirBlock(new BlockPos(newX, newY, newZ))) {
 						IBlockState topBlock = world.getBiomeGenForCoords(new BlockPos(newY, 0, newZ)).topBlock;
 						IBlockState fillerBlock = world.getBiomeGenForCoords(new BlockPos(newX, 0, newZ)).fillerBlock;
